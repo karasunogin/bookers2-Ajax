773ec72df3db770c5ab44e1ca75ae64534c3a779
karasunogin <127410744+karasunogin@users.noreply.github.com>
2023-07-16 06:12:16 +0000

    過去一週間でいいねの合計カウントが多い順に投稿を表示

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,12 +9,13 @@ class BooksController < ApplicationController
   end
 
   def index
-    to = Time.current.at_end_of_day
-    from = (to - 6.day).at_beginning_of_day
-    @books = Book.includes(:favorited_users).
-      sort_by {|x|
-        x.favorited_users.includes(:favorites).where(created_at: from...to).size
-      }.reverse
+    # 過去一週間でいいねの合計カウントが多い順に投稿を表示
+    to  = Time.current.at_end_of_day
+    from  = (to - 6.day).at_beginning_of_day
+    @books = Book.all.sort {|a,b|
+      b.favorites.where(created_at: from...to).size <=>
+      a.favorites.where(created_at: from...to).size
+    }
     @book = Book.new
   end
 
